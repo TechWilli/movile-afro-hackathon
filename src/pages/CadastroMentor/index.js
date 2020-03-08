9c38a7d647854c96a809709cc4865d4662c4a1f0
William Assis <williamf1f2@gmail.com>
2020-03-08 05:36:02 -0300

    Estilizar cadastro mentor

--- src/pages/CadastroMentor/index.js
+++ src/pages/CadastroMentor/index.js
@@ -12,11 +12,36 @@ class CadastroMentor extends Component {
   render() {
     return (
       <div className="">
-        <div style={{ height: 30, backgroundColor: '#FF5B01' }}></div>
-        <div className="container text-center mt-4">
+        <div style={{ height: 25, backgroundColor: '#FF5B01' }}></div>
+        <div className="container text-center my-4">
           <h4 className="font-weight-bold">Vamos começar o cadastro ; )</h4>
         </div>
-      </div>
+
+        <form className="container mx-auto" style={{ width: 350 }}>
+          <div class="form-group">
+            <label for="inputEmail">Email address</label>
+            <input type="email" class="form-control" id="inputEmail" aria-describedby="emailHelp" />
+            <small id="emailHelp" class="form-text text-muted">nomesobrenome@email.com</small>
+          </div>
+
+          <div class="form-group">
+            <label for="inputCPF">CPF</label>
+            <input type="password" class="form-control" id="inputCPF" />
+          </div>
+
+          <div class="form-group">
+            <label for="inputPassword">Password</label>
+            <input type="password" class="form-control" id="inputPassword" />
+          </div>
+
+          <div class="form-group">
+            <label for="inputPasswordConfirm">Confirm password</label>
+          <input type="password" class="form-control" id="inputPasswordConfirm" />
+          </div>
+        <button type="submit" class="btn btn-primary">Enviar</button>
+        </form>
+
+      </div >
     );
   }
 
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -10,14 +10,14 @@ function Home() {
           <img className="m-0-auto img-responsive mb-4 py-4" src={require("../../assets/logo-triunfo.png")} />
         </div>
         <div className="bg-white shadow m-5 p-4" style={{ borderRadius: 20 }}>
-          <div className="" style={{ marginTop: 20, width: '100%' }}>
-            <p>Acreditamos que o compartilhamento Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Mussum Ipsum, cacilds vidis litro abertis. Não sou faixa preta cumpadi, sou preto inteiris, inteiris. Em pé sem cair, deitado sem dormir, sentado sem cochilar e fazendo pose. Quem num gosta di mé, boa gentis num é. Si u mundo tá muito paradis? Toma um mé que o mundo vai girarzis!</p>
+          <div className="text-center" style={{ marginTop: 20, width: '100%' }}>
+            <p>Nós sabemos a importância de se conectar com os pessoas, e nada melhor do que uma história parecida para juntar duas ou mais pessoas. É isso queremos oferecer para você, uma mentoria com uma pessoa que entende sua história.</p>
           </div>
 
           <div className="mx-auto mt-5" style={{ width: 220 }}>
             <Link className="pt-3 mb-4 d-block btn text-white font-weight-bold" style={{ width: 220, height: 60, borderRadius: 30, backgroundColor: "#FF5B01" }} to="/cadastro-mentor">Quero ser mentor</Link>
             <Link className="pt-3 mb-4 d-block btn text-white font-weight-bold" style={{ width: 220, height: 60, borderRadius: 30, backgroundColor: "#FF5B01" }} to="/cadastro-mentorado">Quero ser mentorado</Link>
-            <Link className="mb-4 d-block btn font-weight-bold shadow-none " style={{ width: 220, borderRadius: 20,  }} to="/login">Já tenho cadastro</Link>
+            <Link className="mb-4 d-block btn font-weight-bold shadow-none" style={{ width: 220, borderRadius: 20 }} to="/login">Já tenho cadastro</Link>
           </div>
         </div>
 
