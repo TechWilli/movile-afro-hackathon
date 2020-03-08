6604c63ec767b8b3f39839696a63e054631455ff
William Assis <williamf1f2@gmail.com>
2020-03-08 04:58:43 -0300

    Estilizar home page

--- src/pages/CadastroMentor/index.js
+++ src/pages/CadastroMentor/index.js
@@ -1,14 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './index.scss';
 
-function CadastroMentor() {
-  return (
-    <div className="CadastroMentor">
-      <div className="container"> 
-      CadastroMentor
+class CadastroMentor extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+
+  render() {
+    return (
+      <div className="">
+        <div style={{ height: 30, backgroundColor: '#FF5B01' }}></div>
+        <div className="container text-center mt-4">
+          <h4 className="font-weight-bold">Vamos começar o cadastro ; )</h4>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
+
 }
 
 export default CadastroMentor;
